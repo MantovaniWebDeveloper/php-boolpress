81ce618c2ff31cbe890ec0c08fbe54ebd1a250ca
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-01 15:14:00 +0100

    struttura html css sass dei commenti fatta ora passiamo alla logica

--- post-detail.php
+++ post-detail.php
@@ -34,6 +34,17 @@
            </div>
            <small>Tag: <a href="#"><?php echo leggiTag($post["tag"]); ?></a></small>
          </div>
+         <div class="wrapPostCommenti">
+           <div class="titleComment">
+             <h2>Comment name</h2>
+           </div>
+           <div class="emailComment">
+             <small>email comment</small>
+           </div>
+           <div class="contentComment">
+             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+           </div>
+         </div>
        <?php } ?>
        <?php endforeach; ?>
      </div>
