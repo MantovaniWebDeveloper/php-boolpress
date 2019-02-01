bd8b1ef417f5a8be634fe5d93a8f1732e3bc06a3
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-01 10:37:21 +0100

    inserito if per differenziare il dettaglio degli articoli

--- post-detail.php
+++ post-detail.php
@@ -1,8 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="src/css/style.css">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      include("data.php");
+      include("functions.php");
+      $getSlug = $_GET["slug"];
+     ?>
 
-  include("data.php");
-  include("functions.php");
+     <div class="container">
+       <?php foreach ($posts as $k => $post) : ?>
+         <?php if($getSlug == $post[slug]) { ?>
+         <div class="wrapPost">
+           <div class="titlePostDetail">
+             <a href="post-detail.php?slug=<?php echo $post[slug]; ?>">
+               <h2><?php echo $post[title]; ?></h2>
+             </a>
+           </div>
+           <div class="datePostDetail">
+             <small>Pubblicato il <?php echo convertiData($post[published_at]); ?></small>
+           </div>
+           <div class="contentPostDetail">
+             <p><?php echo limitaPost($post[content]); ?></p>
+           </div>
+         </div>
+       <?php } ?>
+       <?php endforeach; ?>
+     </div>
 
-  $getSlug = $_GET["slug"];
-  echo $getSlug;
- ?>
+
+  </body>
+</html>
