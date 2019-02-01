ce7865a747f2823a09e986a9ea0ab439d8f13773
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-01 14:39:23 +0100

    risolto il promblema del filtro senza nessun parametro

--- post.php
+++ post.php
@@ -10,11 +10,15 @@
     include("data.php");
     include("functions.php");
     $queryTag = $_GET["tag"];
+    $urlStringa = "http://localhost/php/php-boolpress/post.php";
+    $urlAttuale = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+    var_dump($urlAttuale);
     ?>
 
     <div class="container">
 
       <?php foreach ($posts as $k => $post) : ?>
+        <?php if($queryTag != NULL) { ?>
         <?php foreach ($post["tag"] as $key => $elementoTag) : ?>
         <?php if($queryTag == $elementoTag) { ?>
             <div class="wrapPost">
@@ -30,10 +34,23 @@
                 <p><?php echo limitaPost($post["content"]); ?></p>
               </div>
             </div>
-          <?php } else { ?>
-
           <?php } ?>
         <?php endforeach; ?>
+        <?php } else { ?>
+          <div class="wrapPost">
+            <div class="titlePost">
+              <a href="post-detail.php?slug=<?php echo $post["slug"]; ?>">
+                <h2><?php echo $post["title"]; ?></h2>
+              </a>
+            </div>
+            <div class="datePost">
+              <small>Pubblicato il <?php echo convertiData($post["published_at"]); ?></small>
+            </div>
+            <div class="contentPost">
+              <p><?php echo limitaPost($post["content"]); ?></p>
+            </div>
+          </div>
+      <?php } ?>
       <?php endforeach; ?>
     </div>
 
