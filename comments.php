247f044a2b561ee2cb222ff374e98a8524fda0c0
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-01 16:19:40 +0100

    ok controllo slug per parsare solo l'array in questione fatto(per ora)

--- comments.php
+++ comments.php
@@ -8,11 +8,21 @@
   foreach ($comments as $k => $commenti) {
     echo $k;
     echo "<br />";
-    if($getSlug == $k) {
-      
-      }
-
+    if ($k == $getSlug){
+    foreach ($commenti as $key => $commentoCompleto) {
+      echo "<pre>";
+      var_dump($commentoCompleto);
+      echo "</pre>";
+      echo "<br />";
+      $commentJson = json_encode($commentoCompleto);
+        echo "<br />";
+          echo "<br />";
+      echo "<pre>";
+      var_dump($commentJson);
+      echo "</pre>";
     }
+}
+
 
   }
 
