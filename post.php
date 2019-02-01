648fe53476f0306ca4515edb0c1cedbf83d2ecc3
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-01 14:05:02 +0100

    impostato i filtri con $_GET

--- post.php
+++ post.php
@@ -9,24 +9,31 @@
     <?php
     include("data.php");
     include("functions.php");
+    $queryTag = $_GET["tag"];
     ?>
 
     <div class="container">
 
       <?php foreach ($posts as $k => $post) : ?>
-        <div class="wrapPost">
-          <div class="titlePost">
-            <a href="post-detail.php?slug=<?php echo $post["slug"]; ?>">
-              <h2><?php echo $post["title"]; ?></h2>
-            </a>
-          </div>
-          <div class="datePost">
-            <small>Pubblicato il <?php echo convertiData($post["published_at"]); ?></small>
-          </div>
-          <div class="contentPost">
-            <p><?php echo limitaPost($post["content"]); ?></p>
-          </div>
-        </div>
+        <?php foreach ($post["tag"] as $key => $elementoTag) : ?>
+        <?php if($queryTag == $elementoTag) { ?>
+            <div class="wrapPost">
+              <div class="titlePost">
+                <a href="post-detail.php?slug=<?php echo $post["slug"]; ?>">
+                  <h2><?php echo $post["title"]; ?></h2>
+                </a>
+              </div>
+              <div class="datePost">
+                <small>Pubblicato il <?php echo convertiData($post["published_at"]); ?></small>
+              </div>
+              <div class="contentPost">
+                <p><?php echo limitaPost($post["content"]); ?></p>
+              </div>
+            </div>
+          <?php } else { ?>
+
+          <?php } ?>
+        <?php endforeach; ?>
       <?php endforeach; ?>
     </div>
 
